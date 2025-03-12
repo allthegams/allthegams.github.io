974cd2d435869ba0b8857d77a9c4ec4b6126ad42
ebeket <probikeparkrider@gmail.com>
2025-03-11 21:10:09 -0700

    Initial

---
+++ js/gams.js
@@ -0,0 +1,39 @@
+fetch('../assets/gams.json')
+  .then(response => response.json())
+  .then(itemsData => {
+    const gridContainer = document.getElementById('grid-container');
+    const searchBar = document.getElementById('gamSearch');
+
+    const updateGrid = (query = '') => {
+      gridContainer.innerHTML = '';
+      const filteredItems = itemsData.filter(item => 
+        item.subtext.toLowerCase().includes(query.toLowerCase())
+      );
+      filteredItems.forEach(item => {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        gridItem.style.backgroundImage = `url(${item.imgSrc})`;
+
+        const subtext = document.createElement('p');
+        subtext.classList.add('subtext');
+        subtext.textContent = item.subtext;
+
+        gridItem.addEventListener('click', () => {
+          window.location.href = item.source;
+        });
+
+        gridItem.appendChild(subtext);
+        gridContainer.appendChild(gridItem);
+      });
+    };
+
+    updateGrid();
+
+    searchBar.addEventListener('input', (e) => {
+      const query = e.target.value;
+      updateGrid(query);
+    });
+  })
+  .catch(error => {
+    console.error('Error loading games', error);
+  });
